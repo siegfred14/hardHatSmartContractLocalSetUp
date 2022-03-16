cf0fbeead2f869ef87f130e290a746f309605b2c
Siegfred Samson <siegfredsamson@gmail.com>
2022-03-16 15:00:17 +0100

    task balance added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -11,8 +12,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 task("balance", "Get ETH balance of an address")
-  .addParam("address", "Address whose balance is to be fetched")
-  .setAction(async () => {});
+  .addParam("address", "Address which balance is to be fetched")
+  .setAction(async ({ address }) => {
+    // console.log(address)
+    const balance = await web3.eth.getBalance(address);
+    console.log(`Balance is ${web3.utils.fromWei(balance)} ETH`);
+  });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
